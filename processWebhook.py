97e4ca51536bb956445fa994c1847ef79b211e2e
KelvinWong <wongwaikit2873@gmail.com>
2022-03-02 10:12:53 +0800

    init

--- processWebhook.py
+++ processWebhook.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, render_template_string, jsonify
 from flask_cors import CORS, cross_origin
-
+import requests
+import json
 app = Flask(__name__)
 CORS(app)
+
+
+
+
+
 
 @app.route("/", methods=['POST','GET'])
 @cross_origin(supports_credentials=True)
@@ -10,11 +16,36 @@ CORS(app)
 def index():
     Load_Energy = []
     sent_data = request.get_json()
-    print(sent_data[1])
-    for row in sent_data[0]:
-        Load_Energy.append(row['PV Energy'] + row['Utility Import Energy'] - row['Utility Export Energy'])
-    # print(Load_Energy)
+    print(sent_data)
 
+    url = 'http://asplm-cloud.ddns.net:8080/Thingworx/Things/SolarSentryDB/Services/api_DashboardChart_Energy_30Min'
+    appKey = str('ee4b6afb-30f5-4b1d-82ea-ceab37ba7620')
+    SiteID = str(2)
+    FromDT = str(sent_data[3])
+    ToDT = str(sent_data[4])
+    r = requests.Request(method='POST',
+                         url=url + '?appKey=' + appKey + '&SiteID=' + SiteID + '&FromDT=' + FromDT + '&ToDT=' + ToDT,
+                         headers={
+                             'Content-Type': 'application/json',
+                             'Accept': 'application/json',
+                             'Token': 'ee4b6afb-30f5-4b1d-82ea-ceab37ba7620'
+                         }).prepare()
+
+    s = requests.Session()
+    rsp = s.send(r)
+    data = rsp.json()
+
+    s.close()
+
+    a = 1
+    days = []
+    for row in data['rows']:
+        Load_Energy.append(row['kWh_Import_PV'] + row['kWh_Import_TNB'] - row['kWh_Export_PV'])
+        if a == 48:
+            days.append(row['GroupLabel'][:11])
+            a = 0
+        a += 1
+    print(days)
     a = 1
     day = 1
     Total_day = []
@@ -39,7 +70,7 @@ def index():
         i += 1
 
 
-    MD_Reduction = int(sent_data[3])
+    MD_Reduction = int(sent_data[2])
 
     MD = []
     i = 0
@@ -48,14 +79,14 @@ def index():
         i += 1
 
 
-    Total = 100 - MD_Reduction
+    Total = 100 - int(sent_data[2])
     MD_Threshold = []
 
     i = 0
     while len(MD) > i:
         MD_Threshold.append(MD[i] * Total / 100)
         i += 1
-    print("MD Threshold: " + str(MD_Threshold) + "(kW)")
+    # print("MD Threshold: " + str(MD_Threshold) + "(kW)")
 
     More_than_Energy = []  # kWh
     i = 0
@@ -91,7 +122,7 @@ def index():
         MD_Charges.append(MD[i] * perk_period_price)
         i += 1
 
-    Each_Battery_kWh_Cost = int(sent_data[1])  # USD/kWh
+    Each_Battery_kWh_Cost = int(sent_data[0])  # USD/kWh
     RM = 4.2  # 1USD = RM4.2
     Battery_Cost = []
 
@@ -100,7 +131,7 @@ def index():
         Battery_Cost.append(Battery_capacity[i] * Each_Battery_kWh_Cost * RM)
         i += 1
 
-    Each_Inverter_kW_Cost = int(sent_data[2])  # USD/kW
+    Each_Inverter_kW_Cost = int(sent_data[1])  # USD/kW
     Inverter_Cost = []
     b = 0
 
@@ -167,75 +198,64 @@ def index():
     Total_Saving_Day = []
     i = 0
     while len(MD_Saving_Per_Year) > i:
-        Total_Saving_Day.append(MD_Saving_Per_Day[i] + Energy_Trading_profit_per_day[i])
+        Total_Saving_Day.append(round(MD_Saving_Per_Day[i] + Energy_Trading_profit_per_day[i],2))
         i += 1
 
-    return ({'roi':ROI, 'day': Total_day, 'saving_per_day': Total_Saving_Day})
+    return ({'roi':ROI, 'day': days, 'saving_per_day': Total_Saving_Day, 'battery_capacity': Battery_capacity, 'investment':Investment, 'saving_per_year':MD_Saving_Per_Year })
+
+@app.route("/Information", methods=['POST','GET'])
+@cross_origin(supports_credentials=True)
+
+def Information():
+    sent_data = request.get_json()
+    url = 'http://asplm-cloud.ddns.net:8080/Thingworx/Things/SolarSentryDB/Services/api_DashboardChart_Energy'
+    appKey = str('ee4b6afb-30f5-4b1d-82ea-ceab37ba7620')
+    SiteID = str(2)
+    InDate = sent_data[0]
+    GroupBy = sent_data[1]
+
+    print(sent_data)
+    r = requests.Request(method='POST',
+                         url=url + '?appKey=' + appKey + '&SiteID=' + SiteID + '&InDate=' + InDate + '&GroupBy=' + GroupBy,
+                         headers={
+                             'Content-Type': 'application/json',
+                             'Accept': 'application/json',
+                             'Token': 'ee4b6afb-30f5-4b1d-82ea-ceab37ba7620'
+                         }).prepare()
+
+    s = requests.Session()
+    rsp = s.send(r)
+    data = rsp.json()
+    s.close()
+
+    day = []
+    time = []
+    kWh_Export_PV = []
+    kWh_Import_TNB = []
+    kWh_Import_PV = []
+    kW_Import_TNB = []
+    kW_Import_PV = []
+    load = []
+    for row in data['rows']:
+        # print(row)
+        kWh_Export_PV.append(row['kWh_Export_PV'])
+        kWh_Import_TNB.append(row['kWh_Import_TNB'])
+        kWh_Import_PV.append(row['kWh_Import_PV'])
+        kW_Import_TNB.append(row['kWh_Import_TNB'] * 2)
+        kW_Import_PV.append(row['kWh_Import_PV'] * 2)
+        if(sent_data[1] == 'Month'):
+            time.append(row['GroupLabel'])
+        elif(sent_data[1] == 'Year'):
+            time.append(row['GroupLabel'][5:7])
+        else:
+            time.append(row['GroupLabel'][11:16])
+        day.append(row['GroupLabel'][:11])
+        load.append((row['kWh_Import_PV'] * 2) + (row['kWh_Import_TNB'] * 2))
+
+    # print(time)
+    return({"kWh_Export_PV": kWh_Export_PV, "kWh_Import_TNB": kWh_Import_TNB, "kWh_Import_PV": kWh_Import_PV,"kW_Import_PV": kW_Import_PV, "kW_Import_TNB": kW_Import_TNB,"load": load, 'day': day, 'time': time})
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
-# @app.route("/", methods=['POST','GET'])
-# @cross_origin(supports_credentials=True)
-#
-# def index():
-#     Load_Energy = []
-#     sent_data = request.get_json()
-#     for row in sent_data:
-#         Load_Energy.append(row['PV Energy'] + row['Utility Import Energy'] - row['Utility Export Energy'])
-#     # print(Load_Energy)
-#     print(max(Load_Energy))
-#
-#     MD_Reduction = 10
-#
-#     MD = max(Load_Energy) * 2
-#
-#     Total = 100 - MD_Reduction
-#     MD_Threshold = MD * Total / 100
-#
-#     Threshold = MD_Threshold / 2  # kWh
-#
-#     Battery_Discharged_Energy_Each_30 = []
-#
-#     for i in Load_Energy:
-#         if i > Threshold:
-#             Battery_Discharged_Energy_Each_30.append(i - Threshold)
-#
-#     Battery_Discharged_Energy = sum(Battery_Discharged_Energy_Each_30)
-#
-#     BESS_Capacity = Battery_Discharged_Energy / 0.9
-#
-#     Each_Battery_kWh_Cost = 305 #USD/kWh
-#     Battery_Cost = Each_Battery_kWh_Cost * 4.2 * BESS_Capacity
-#
-#     Each_Inverter_kW_Cost = 90 #USD/kW
-#     Inverter_Cost = max(Battery_Discharged_Energy_Each_30) * 1.5 * Each_Inverter_kW_Cost * 4.2 * 2
-#
-#     Investment = Battery_Cost + Inverter_Cost
-#     print("Investment: RM" + str(Investment))
-#
-#     perk_period_price = 45.1  # For each kilowatt of maximum demand per month during the peak period
-#     MD_Charges = MD * perk_period_price  # RM
-#     print("MD Charges RM" + str(MD_Charges))
-#
-#     Month = 12  # 1 Year How many month
-#     MD_Saving_Per_Year = MD_Charges * MD_Reduction / 100 * Month
-#     MD_Saving_Per_Year = round(MD_Saving_Per_Year, 2)
-#     print("MD Saving per year: RM" + str(MD_Saving_Per_Year))
-#
-#     Save = 0.141  # 0.141 is the price difference between the peak and off peak tariff, we will basically save 0.141 per kWh
-#     Day = 365  # 1 Year How many day?
-#     Energy_Trading_profit_per_year = Battery_Discharged_Energy * Save * Day
-#     Energy_Trading_profit_per_year = round(Energy_Trading_profit_per_year, 2)
-#     print("Energy Trading profit per year: RM" + str(Energy_Trading_profit_per_year))
-#
-#     Total_Saving = MD_Saving_Per_Year + Energy_Trading_profit_per_year
-#     Total_Saving = round(Total_Saving, 2)
-#     print("Total Saving: RM" + str(Total_Saving))
-#
-#     ROI = Investment / Total_Saving
-#     ROI = round(ROI, 2)
-#     print(ROI)
-#
-#     return ({'yes':'wong'})
